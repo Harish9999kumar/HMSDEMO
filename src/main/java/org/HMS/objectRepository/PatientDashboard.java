b35de94f2487dcc5acf3efc92d8aebb4d126d00a
Harish9999kumar <97820952+Harish9999kumar@users.noreply.github.com>
2022-09-21 10:59:21 +0530

    new

--- src/main/java/org/HMS/objectRepository/PatientDashboard.java
+++ src/main/java/org/HMS/objectRepository/PatientDashboard.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-//something yoiu goive
 public class PatientDashboard {
 	public PatientDashboard(WebDriver driver) {
 		PageFactory.initElements(driver, this);
